a9b1cb001c60d480e0c26000cf6093e8a1212433
bibletoon <alex8.ivanov@gmail.com>
2023-06-13 00:16:26 +0300

    Fix: decomposed shape factory tests

--- AreaCalculator.Tests/Shapes/ShapeFactoryTests.cs
+++ AreaCalculator.Tests/Shapes/ShapeFactoryTests.cs
@@ -40,18 +40,41 @@ public class ShapeFactoryTests
         });
     }
 
-    [TestCase(5)]
-    [TestCase(3, 4, 5)]
-    public void ShapeFactory_Create_CreatesShape(params double[] parameters)
+    [TestCase(new double[]{})]
+    [TestCase(3, 4)]
+    [TestCase(1, 2, 3, 4)]
+    public void ShapeFactory_CreateWithInvalidArgumentsCount_ThrowsArgumentException(params double[] parameters)
     {
         var shapeFactory = new ShapeFactory();
         
+        Assert.That(() => shapeFactory.Create(parameters), Throws.ArgumentException);
+    }
+
+    [TestCase(5)]
+    public void ShapeFactory_CreateWithOneArgument_CreatesCircle(double parameter)
+    {
+        var shapeFactory = new ShapeFactory();
+
+        var shape = shapeFactory.Create(parameter);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(shape, Is.Not.Null);
+            Assert.That(shape, Is.InstanceOf<Circle>());
+        });
+    }
+    
+    [TestCase(3, 4, 5)]
+    public void ShapeFactory_CreateWithMultipleArguments_CreatesTriangle(params double[] parameters)
+    {
+        var shapeFactory = new ShapeFactory();
+
         var shape = shapeFactory.Create(parameters);
         
         Assert.Multiple(() =>
         {
             Assert.That(shape, Is.Not.Null);
-            Assert.That(shape, Is.InstanceOf<IShape>()); 
+            Assert.That(shape, Is.InstanceOf<Triangle>());
         });
     }
 }
